ba44500fa74393e22ea8c96ae397e461a2eee207
Weidong Sun <sunweidong90@gmail.com>
2020-03-08 18:50:23 -0500

    udpate Reward object str

--- backend/budgetmgr/models/account.py
+++ backend/budgetmgr/models/account.py
@@ -62,7 +62,7 @@ class Reward(models.Model):
 
     def __str__(self):
         if self.end_time:
-            active_period = 'TBD'
+            active_period = f'{self.end_time.year}{self.end_time.month}'
         else:
             active_period = "4ever"
         return f"{self.account}|{active_period}|{self.reward_type}"
