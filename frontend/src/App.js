b132ac6718fb4eaf4274a6eee7ce0cfebf0cf155
Weidong Sun <sunweidong90@gmail.com>
2020-03-08 22:03:51 -0500

    add material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+          <CssBaseline/>
+          <Button variant="contained" color={"primary"}>
+              Hello XiaoManager
+          </Button>
+      </React.Fragment>
+
   );
 }
 
