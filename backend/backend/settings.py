d0655b306b823ed4916f63038a1facbf159bfeb6
Weidong Sun <sunweidong90@gmail.com>
2020-03-11 21:43:09 -0500

    backend add pygraphviz package

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
     'budgetmgr'
 ]
