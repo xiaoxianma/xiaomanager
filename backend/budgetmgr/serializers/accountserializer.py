8ab57ef781cddd61e2c125de9d709716cd100eac
Weidong Sun <sunweidong90@gmail.com>
2020-03-12 19:43:51 -0500

    add more serializer

--- backend/budgetmgr/serializers/accountserializer.py
+++ backend/budgetmgr/serializers/accountserializer.py
@@ -1,5 +1,38 @@
 from rest_framework import serializers
-from budgetmgr.models.account import Reward
+from budgetmgr.models.account import (
+    Reward, Institution, AccountType, AccountOwner,
+    Account, RewardType
+)
+
+
+class InstitutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Institution
+        fields = '__all__'
+
+
+class AccountTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountType
+        fields = '__all__'
+
+
+class AccountOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountOwner
+        fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+
+class RewardTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        mode = RewardType
+        fields = '__all__'
 
 
 class RewardSerializer(serializers.ModelSerializer):
