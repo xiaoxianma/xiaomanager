a8d7c38f986e9af2b268dde08a8b94d31ae6d2dd
Weidong Sun <sunweidong90@gmail.com>
2020-03-12 20:07:07 -0500

    update accountseriliazer

--- backend/budgetmgr/serializers/accountserializer.py
+++ backend/budgetmgr/serializers/accountserializer.py
@@ -9,35 +9,43 @@ class InstitutionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Institution
         fields = '__all__'
+        exclude = ['id']
 
 
 class AccountTypeSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='get_name_display')
+
     class Meta:
         model = AccountType
         fields = '__all__'
+        exclude = ['id']
 
 
 class AccountOwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountOwner
         fields = '__all__'
+        exclude = ['id']
 
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = '__all__'
+        exclude = ['id']
 
 
 class RewardTypeSerializer(serializers.ModelSerializer):
     class Meta:
         mode = RewardType
         fields = '__all__'
+        exclude = ['id']
 
 
 class RewardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reward
         fields = '__all__'
+        exclude = ['id']
         read_only_fields = ['account', 'reward_type']
         depth = 3
