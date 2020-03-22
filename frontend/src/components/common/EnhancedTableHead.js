fa2946b01dc763e8eb0819897c473759f51eab7c
Weidong Sun <sunweidong90@gmail.com>
2020-03-22 15:54:10 -0500

    fix cc benefits table head color

--- frontend/src/components/common/EnhancedTableHead.js
+++ frontend/src/components/common/EnhancedTableHead.js
@@ -9,9 +9,25 @@ import {makeStyles, withStyles} from "@material-ui/core/styles";
 const StyledTableCell = withStyles(theme => ({
     head: {
         backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    }
+    },
 }))(TableCell);
+
+
+const StyledTableSortLabel = withStyles(theme => ({
+    root: {
+        color: theme.palette.common.white,
+        "&:hover": {
+            color: theme.palette.common.white,
+        },
+        '&$active': {
+            color: theme.palette.common.white,
+        },
+    },
+    active: {},
+    icon: {
+        color: 'inherit !important'
+    },
+}))(TableSortLabel);
 
 
 const useStyles = makeStyles(theme => ({
@@ -26,9 +42,6 @@ const useStyles = makeStyles(theme => ({
         top: 20,
         width: 1,
     },
-    tableSortLabel: {
-        color: theme.palette.common.white,
-    }
 }));
 
 export default function EnhancedTableHead(props) {
@@ -47,8 +60,7 @@ export default function EnhancedTableHead(props) {
                         key={headCell.id}
                         sortDirection={orderBy === headCell.id ? order : false}
                     >
-                        <TableSortLabel
-                            className={classes.tableSortLabel}
+                        <StyledTableSortLabel
                             active={orderBy === headCell.id}
                             direction={orderBy === headCell.id ? order : 'asc'}
                             onClick={createSortHandler(headCell.id)}
@@ -59,7 +71,7 @@ export default function EnhancedTableHead(props) {
                                     {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                                 </span>
                             ) : null}
-                        </TableSortLabel>
+                        </StyledTableSortLabel>
                     </StyledTableCell>
                 ))}
             </TableRow>
