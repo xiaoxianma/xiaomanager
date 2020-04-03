bea05795a17a0ea151bf3edcb6adb5b8c1c1a600
Weidong Sun <sunweidong90@gmail.com>
2020-04-03 00:25:22 -0500

    fix heroku logging bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -206,7 +206,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Configure app for Heroku deployment
-django_heroku.settings(locals())
+django_heroku.settings(locals(), logging=False)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
