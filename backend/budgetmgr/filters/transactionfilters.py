cad5d8dd4bdb992d85da50478bfcc2c69e8dc8f3
Weidong Sun <sunweidong90@gmail.com>
2020-04-10 14:08:04 -0500

    fix transaction filter bug

--- backend/budgetmgr/filters/transactionfilters.py
+++ backend/budgetmgr/filters/transactionfilters.py
@@ -3,8 +3,8 @@ from budgetmgr.models.transaction import Transaction
 
 
 class TransactionFilter(filters.FilterSet):
-    min_date = filters.NumberFilter(field_name='transaction_date', lookup_expr='gte')
-    max_date = filters.NumberFilter(field_name='transaction_date', lookup_expr='lte')
+    min_date = filters.IsoDateTimeFilter(field_name='transaction_date', lookup_expr='gte')
+    max_date = filters.IsoDateTimeFilter(field_name='transaction_date', lookup_expr='lte')
 
     class Meta:
         model = Transaction
