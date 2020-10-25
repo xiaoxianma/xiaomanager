5f61b14cc6590309988d7af4617d5978bffd22ba
Weidong Sun <sunweidong90@gmail.com>
2020-10-25 10:12:03 -0500

    add OAuth2 + jwt security

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,11 +1,23 @@
 import os
+import typing as t
 from datetime import datetime, timedelta
 
 import jwt
-from fastapi.security import OAuth2PasswordBearer
+from app.core import security
+from app.db import session
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 from passlib.context import CryptContext
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/token",
+    scopes= {
+        "server_management": "Edit server settings",
+        "token_management": "Create, list or edit tokens",
+        "notification_management": "Create, list or edit notification providers",
+        "full_control": "Full control over what current user has",
+    }
+)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -34,3 +46,47 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+class AuthDependency:
+
+    def __init__(self, enabled: bool = True, token: t.Optional[str] = None):
+        self.enabled = enabled
+        self.token = token
+
+    def raise_unauthorized_exception(self, authenticate_value):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": authenticate_value},
+        ) 
+
+    def raise_forbidden_exception(self, authenticate_value):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+            headers={"WWW-Authenticate": authenticate_value},
+        ) 
+
+    async def __call__(self, request: Request, security_scopes: SecurityScopes, return_token=False, db=Depends(session.get_db)):
+        from app.db.crud.user import get_user_by_email
+        if not self.enabled:
+            return None
+        if security_scopes.scopes:
+            authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+        else:
+            authenticate_value = "Bearer"
+        token: str = await oauth2_scheme(request) if not self.token else self.token
+        if token is None: self.raise_unauthorized_exception(authenticate_value)
+        try:
+            payload = jwt.decode(token, security.SECRET_KEY, algorithm=[security.ALGORITHM])
+        except (jwt.PyJWTError, jwt.ExpiredSignatureError) as e:
+            self.raise_unauthorized_exception(authenticate_value)
+        email: str = payload.get('sub')
+        permission: str = payload.get('permissions')
+        user = get_user_by_email(db, email)
+        if user is None:
+            self.raise_forbidden_exception(authenticate_value)
+        if permission is None or permission not in ['admin', 'user']:
+            self.raise_forbidden_exception(authenticate_value)
+        return user, token if return_token else user
--- backend/app/utils/view.py
+++ backend/app/utils/view.py
@@ -1,9 +1,10 @@
 import typing as t
 
 import app.db.crud.utils as crud_utils
-from app.core import security
+from app.core.security import AuthDependency
 from app.db.session import Base, get_db
-from fastapi import APIRouter, FastAPI
+from app.db.schemas import user as user_schema
+from fastapi import APIRouter, FastAPI, Security
 from fastapi.params import Depends
 from pydantic import BaseModel
 
@@ -18,35 +19,34 @@ class ModelViewSet:
 
     def __init__(self):
         self.r = APIRouter()
-        self.oauth2_scheme = security.oauth2_scheme
 
     def register(self):
         @self.r.get(f"/{self.ENDPOINT}s", response_model=t.List[self.GET_SCHEMA_OUT])
-        async def get_all(token: str = Depends(self.oauth2_scheme), db=Depends(get_db)):
+        async def get_all(user: user_schema.UserBase = Security(AuthDependency()), db=Depends(get_db)):
             return crud_utils.get_all_items(db, self.MODEL)
 
         @self.r.get(
             f"/{self.ENDPOINT}/" + "{inst_id}", response_model=self.GET_SCHEMA_OUT
         )
-        async def get(inst_id: int, token: str = Depends(self.oauth2_scheme), db=Depends(get_db)):
+        async def get(inst_id: int, user: user_schema.UserBase = Security(AuthDependency()), db=Depends(get_db)):
             return crud_utils.get_item(db, self.MODEL, inst_id)
 
         @self.r.post(
             f"/{self.ENDPOINT}", response_model=self.GET_SCHEMA_OUT, status_code=201
         )
-        async def create(payload: self.POST_SCHEMA_IN, token: str = Depends(self.oauth2_scheme), db=Depends(get_db)):
+        async def create(payload: self.POST_SCHEMA_IN, user: user_schema.UserBase = Security(AuthDependency()), db=Depends(get_db)):
             return crud_utils.create_item(db, self.MODEL, payload)
 
         @self.r.patch(
             f"/{self.ENDPOINT}/" + "{inst_id}", response_model=self.GET_SCHEMA_OUT
         )
         async def update(
-            inst_id: int, payload: self.POST_SCHEMA_IN, token: str = Depends(self.oauth2_scheme), db=Depends(get_db)
+            inst_id: int, payload: self.POST_SCHEMA_IN, user: user_schema.UserBase = Security(AuthDependency()), db=Depends(get_db)
         ):
             return crud_utils.update_item(db, self.MODEL, inst_id, payload)
 
         @self.r.delete(f"/{self.ENDPOINT}/" + "{inst_id}", response_model=None)
-        async def delete(inst_id: int, token: str = Depends(self.oauth2_scheme), db=Depends(get_db)):
+        async def delete(inst_id: int, user: user_schema.UserBase = Security(AuthDependency()), db=Depends(get_db)):
             return crud_utils.delete_item(db, self.MODEL, inst_id)
 
 
